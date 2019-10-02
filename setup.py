3d0c09b6291c60871aae6f005e293606dc61084e
Mew-www <mew.proxy@hotmail.com>
2019-10-02 17:35:33 +0300

    Add install dependencies and version info (3.6 preferred)

--- setup.py
+++ setup.py
@@ -3,6 +3,9 @@ import setuptools
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
+with open("requirements.txt", "r") as fh:
+    install_requires = list(fh.read().splitlines())
+
 setuptools.setup(
     name="minnameko-gatherer-utils",
     version="1.0.0",
@@ -13,8 +16,10 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/mew-www/minnameko-gatherer-utils",
     packages=setuptools.find_packages(),
+    install_requires=install_requires,
     classifiers=[
         "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
         "Operating System :: OS Independent",
     ]
 )
